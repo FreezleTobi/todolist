34047cf064220daa2816d85e2757515d85ec2cbb
FreezleTobi <tobias.bumann123@gmail.com>
2022-02-02 14:59:01 +0100

    Fix Flask Server

--- project/todolist.py
+++ project/todolist.py
@@ -2,13 +2,11 @@ from flask import Flask, redirect, url_for, render_template, request
 
 # Flask Server initialisieren
 app = Flask(__name__)
-# Route für die Hauptseite
+# Route fuer die Hauptseite
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
-
 if __name__ == '__main__':
     # starte Flask-Server
-    app.run(host='0.0.0.0', port=4444)
+    app.run(host='0.0.0.0', port=4443)
